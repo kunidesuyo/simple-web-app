8631683318aeef8758781aebceb029cb743fe9e4
kunidesuyo <kunidesuyo1234@gmail.com>
2021-06-22 19:23:32 +0900

    deploy test

--- laravel-docker-workspace/laravel-app/resources/views/welcome.blade.php
+++ laravel-docker-workspace/laravel-app/resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
         </style>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Hello kuni</h1>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
